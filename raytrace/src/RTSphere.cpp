1df8a243289a9e2d2dd0ab6c57d472d38c76e440
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2014-02-11 20:48:51 -0800

    Completed RTSphere shadePoint

--- raytrace/src/RTSphere.cpp
+++ raytrace/src/RTSphere.cpp
@@ -65,30 +65,25 @@ MatrixPtr RTSphere::normal(const Matrix &point, const Matrix &from) {
 }
 
 RTMaterial RTSphere::shadePoint(const Matrix &point) {
-  (void) point;
 
-  Matrix R = point - _origin;
-  
-  float x = R.get(0);
-  float y = R.get(1);
-  float z = R.get(2);
-  float r = _radius;
-
-  float phi = asin(y/r);
-  float theta = acos(x / (r*cos(phi)));
-  float theta2 = asin(z / (r*cos(phi)));
-
-  printf("%f\n", theta);
-  printf("%f\n", theta2);
-  
-
-  float u = theta / (2*M_PI);
-  float v = (phi / M_PI) + 0.5;
-  
   if (_shader) {
-    _shader->shade(u, v, RTMaterial());
+
+    Matrix R = point - _origin;
+  
+    float x = R.get(0);
+    float y = R.get(1);
+    float z = R.get(2);
+    float r = _radius;
+
+    float phi = asin(y/r);
+    float theta = acos(x / (r*cos(phi)));
+    float theta2 = asin(z / (r*cos(phi)));
+
+    float u = theta / (2*M_PI);
+    float v = 1 - ((phi / M_PI) + 0.5);
+    
+    return _shader->shade(u, v, RTMaterial());
   }
 
-
-  return RTMaterial();
+  return getMaterial(0);
 }
